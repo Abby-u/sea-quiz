589a43980cccee052fd5548eb74a76a762c12290
Abby <163426330+Abby-u@users.noreply.github.com>
2024-10-14 12:03:53 +0700

    Update inQuiz.lua

--- states/inQuiz.lua
+++ states/inQuiz.lua
@@ -18,6 +18,7 @@ cancelSound = love.audio.newSource('assets/sounds/cancelMenu.ogg','static')
 seaBgText = love.graphics.newImage('assets/images/inQuiz/seaBgText.png')
 blackBars = love.graphics.newImage('assets/images/inQuiz/blackBars.png')
 cardTest = animx.newActor('assets/images/inQuiz/cardGrey.png'):switch('cardIdle'):loopAll()
+cardhud = animx.newActor('assets/images/inQuiz/cardGrey.png'):switch('cardIdle'):loopAll()
 cardHB = love.graphics.newImage('assets/images/inQuiz/cardHB.png')
 
 local curTime = gVar.bgMusicCurTime
@@ -27,15 +28,16 @@ local lastBeat = 0
 local text1X = 0
 local text2X = 0
 
-local mouseX=love.mouse.getX()
-local mouseY=love.mouse.getY()
+local mouseX = love.mouse.getX()
+local mouseY = love.mouse.getY()
 
 local mouseCardX,mouseCardY=0,0
 
 local camGameX = 0--windowWidth/2
 local camGameY = 0--windowHeight/2
+local camGameZoom = 0.1
 
-local camGame = camera(camGameX,camGameY,0.5)
+local camGame = camera(camGameX,camGameY,camGameZoom)
 
 function love.keypressed(key)
     if gVar.curState == 'inQuiz' then
@@ -43,7 +45,6 @@ function love.keypressed(key)
             cronBack = cron.after(2, function() state.switch('states/mainMenu') end)
             cancelSound:play()
         end
-        testString = key
         
     end
 end
@@ -77,16 +78,81 @@ local cardTestH = cardTest:getHeight()*0.5
 local cardTestR = 0
 local cardTestS = 1
 
+local cardHUDX = 0
+local cardHUDY = 0
+local cardHUDS = camGameZoom
+
+local curCard = 0
+
 cards = {}
 
 cardInfo:initInfo(cardTest:getWidth(),cardTest:getHeight(),marginL,marginU,marginR,marginD,cardTestS)
 
 function drawCards()
     for i=1,#cardInfo.X do
-        cardTest:draw(cardInfo.X[i]-cardInfo.Tx[i],cardInfo.Y[i]-cardInfo.Ty[i],cardTestR,cardInfo.S[i],cardInfo.S[i])
+        if curCard == i then
+            -- dont draw
+        else
+            cardTest:draw(cardInfo.X[i]-cardInfo.Tx[i],cardInfo.Y[i]-cardInfo.Ty[i],cardTestR,cardInfo.S[i],cardInfo.S[i])
+        end
         i=i+1
     end
 end
+
+local cardFlipRot = 0
+local cardFlipX = 0
+local cardFlipY = 0
+local cardFlipTransform = love.math.newTransform(cardFlipX,cardFlipY)
+local cardFlipSize = {1280,720}
+local cardFlipShader = love.graphics.newShader(
+[[// Angle in radians.
+uniform float a;
+
+// Transform that you want the image to be drawn with.
+uniform mat4 image_tf;
+
+// Image size in pixels.
+uniform vec2 image_size;
+
+vec4 position(mat4 transform_projection, vec4 vertex_position)
+{
+    float xc = image_size.x / 2.0;
+    float yc = image_size.y / 2.0;
+    mat4 origin_mat = mat4(1.0, 0.0, 0.0, 0.0,
+                           0.0, 1.0, 0.0, 0.0,
+                           0.0, 0.0, 1.0, 0.0,
+                           -xc, -yc, 0.0, 1.0);
+
+    mat4 rotate_y_mat = mat4( cos(a), 0.0, sin(a), 0.0,
+                                 0.0, 1.0,    0.0, 0.0,
+                             -sin(a), 0.0, cos(a), 0.0,
+                                 0.0, 0.0,    0.0, 1.0);
+
+    // Setting this to -1.0 will flip the direction of rotation.
+    const float ROTATION_SIGN = -1.0;
+
+    // A factor that controls the foreshortening (the perspective effect).
+    // Suggested numbers: 500.0, 1000.0, 5000.0, 10000.0 etc.
+    const float FORESHORTENING = 1000.0;
+
+    // A large number to keep Z coordinates from clipping out of the screen.
+    // Try setting this to like 10.0 to see why it's needed.
+    const float Z_COMPRESSION = 10000.0;
+
+    mat4 css_projection_mat = mat4(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0 / Z_COMPRESSION, ROTATION_SIGN / FORESHORTENING,
+    0.0, 0.0, 0.0, 1.0);
+
+    mat4 restore_mat = mat4(1.0);
+    restore_mat[3] = vec4(xc, yc, 0.0, 1.0);
+
+    vec4 perspective_position = restore_mat * css_projection_mat * rotate_y_mat
+                                * origin_mat * vertex_position;
+    return transform_projection * image_tf * perspective_position;
+}]]
+)
 
 function cardHoverEffect()
     for i=1,#cardInfo.X do
@@ -98,30 +164,37 @@ function cardHoverEffect()
             cardInfo.S[i] = lerp(cardInfo.S[i],1,0.1)
             cardInfo.Tx[i] = lerp(cardInfo.Tx[i],0,0.1)
             cardInfo.Ty[i] = lerp(cardInfo.Ty[i],0,0.1)
+            --cardFlipRot = lerp(cardFlipRot,0,0.01)
         end
         i=i+1
     end
 end
+
+function moveCardHUD()
+    cardFlipX = lerp(cardFlipX,-200,0.03)
+    cardFlipY = lerp(cardFlipY,10,0.03)
+    cardFlipTransform = love.math.newTransform(cardFlipX,cardFlipY)
+end
+
+function getCurCard()
+    local j = 0
+    for i=1,#cardInfo.X do
+        --curCard = 0
+        if mouseCardX > cardInfo.X[i] and mouseCardX < cardInfo.W[i] and mouseCardY > cardInfo.Y[i] and mouseCardY < cardInfo.H[i] then
+            j = i
+            break
+        end
+        i=i+1
+    end
+    return j
+end
+
 -- cards stuff end
 
---[[ function cardHoverEffect()
-    local tempX = marginL
-    local tempY = marginU
-    if mouseCardX > cardTestX and mouseCardX < cardTestX + cardTest:getWidth() and mouseCardY > cardTestY and mouseCardY < cardTestY + cardTest:getHeight() then
-        cardTestS = lerp(cardTestS,1.1,0.1)
-        --cardTestX = lerp(cardTestX,tempX-cardTestW*1.1,0.1)
-        --cardTestY = lerp(cardTestY,tempY-cardTestH*1.1,0.1)
-    else
-        cardTestS = lerp(cardTestS,1,0.1)
-        --cardTestX = lerp(cardTestX,tempX,0.1)
-        --cardTestY = lerp(cardTestY,tempY,0.1)
-    end
-end ]]
-
-
-
 function love.load()
-    
+    fnf = love.graphics.newFont('assets/fonts/fnf-regular.otf',96)
+    text1 = love.graphics.newText(fnf)
+    text1:addf({{1,1,1},"Test"},windowWidth,'center')
 end
 
 function love.draw()
@@ -148,17 +221,32 @@ function love.draw()
     drawCards()
 
     --[[ for i=1,#cardInfo.X do
-        love.graphics.rectangle('line',cardInfo:getPos(i))
+        
     end ]]
 
     camGame:detach()
+
+    love.graphics.setShader(cardFlipShader)
+    cardFlipShader:send('a', math.rad(cardFlipRot))
+    cardFlipShader:send('image_tf', cardFlipTransform)
+    cardFlipShader:send('image_size', cardFlipSize)
+    --cardTest:draw(640-cardTest:getWidth()/4,360-cardTest:getHeight()/4,cardTestR,cardInfo.S[1]*camGameZoom,cardInfo.S[1]*camGameZoom)
+    cardhud:draw(640-cardhud:getWidth()/2,360-cardhud:getHeight()/2,0,cardHUDS,cardHUDS)
+    love.graphics.setColor(0,0,0)
+    love.graphics.draw(text1,0,150)
+    love.graphics.setColor(1,1,1)
+    love.graphics.setShader()
+
+
+
     love.graphics.draw(blackBars,-50,-64)
     logoBumpin2:draw(windowWidth/2-(logoBumpin2:getWidth()*0.35/2),0,0,0.35,0.35)
+
     love.graphics.print("FPS: ".. love.timer.getFPS(),0,0)
     love.graphics.print("curBeat|lastBeat: ".. curBeat..'|'..lastBeat,0,20)
     love.graphics.print("curTime: ".. curTime,0,40)
     love.graphics.print("mousePosX|mousePosY: ".. mouseCardX..' | '..mouseCardY,0,60)
-    love.graphics.print("cardInfo.S[2]: ".. cardInfo.W[2],0,80)
+    love.graphics.print("cardInfo.S[2]: ".. cardFlipX .. " " .. cardFlipY,0,80)
     
 end
 
@@ -187,8 +275,33 @@ function love.update(dt)
     camGame:lookAt(camGameX,camGameY)
 
     cardHoverEffect()
+    --cardFlipRot = lerp(cardFlipRot,165,0.03)
+    --cardHUDS = lerp(cardHUDS,1,0.03)
+    --moveCardHUD()
+
+
+    --[[ if cardFlipRot > 180 then
+        cardFlipRot = 0
+    else
+        cardFlipRot = cardFlipRot + dt
+    end ]]
 
     lastBeat = curBeat
+end
+
+function love.mousepressed(x, y, button, isTouch)
+    if button == 1 then
+        curCard = getCurCard()
+        if curCard == 0 then 
+        else
+        --cardFlipX,cardFlipY = camGame:cameraCoords(cardInfo.X[curCard],cardInfo.Y[curCard])
+        cardFlipX,cardFlipY = camGame:cameraCoords(cardInfo:getMiddle(curCard))
+        --cardFlipTransform = love.math.newTransform(cardFlipX-cardhud:getWidth()/2*camGameZoom,cardFlipY-cardhud:getHeight()/2*camGameZoom)
+        cardFlipTransform = love.math.newTransform(cardFlipX*camGameZoom,cardFlipY*camGameZoom)
+        cardFlipRot = 0
+        cardHUDS = camGameZoom
+        end
+    end
 end
 
 return inQuiz
